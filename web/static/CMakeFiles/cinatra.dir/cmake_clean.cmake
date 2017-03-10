1e7336a13a9626fc493987044ea759c0c86727e5
Gogs <gogs@fake.local>
2017-03-10 16:38:55 +0800

    add web

---
+++ web/static/CMakeFiles/cinatra.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cinatra.dir/main.cpp.o"
+  "cinatra.pdb"
+  "cinatra"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cinatra.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
