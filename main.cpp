09c2948d3806ed340cf5f2ee7a7859b4ac4b38ce
Gogs <gogs@fake.local>
2017-03-10 16:11:03 +0800

    add project files

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cinatra/cinatra.h>
+#include <ormpp/dbng.hpp>
+#include <iguana/json.hpp>
+
+int main()
+{
+	cinatra::cinatra<> app;
+	app.route("/", [](cinatra::request const& req, cinatra::response& res)
+	{
+		//res.response_file("web/static/index.html");
+		res.response_text("Your IP address is " + req.remote_endpoint().address().to_string());
+	});
+
+	app.set_static_path("./web/static");
+	app.listen("127.0.0.1", "9000");
+	app.run();
+
+	return 0;
+}
